b874048523e8b5bb34f51b7aa5d00f9140f87252
CeoFred <johnsonmessilo19@gmail.com>
2021-01-12 19:59:10 +0100

    done

---
+++ bin/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
+const axios = require("axios");
+
+const stripper = require("./stripper");
+
+const options = yargs
+ .usage("Usage: -d <directory>")
+ .option("-d", { alias: "directory", describe: "Directory To Search for javascript/typescript files", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Stripping your javascript files off logs, searching in ${options.name} Directory`;
+
+
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+const msgBox = boxen( greeting, boxenOptions );
+
+console.log(msgBox);
+
+ stripper.strip(options.d,function(){
+   console.log("Done Clearing log instances");
+ })
+
---
+++ bin/stripper.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+
+var walkPath = './main';
+        var totalFiles = 0;
+        const directorListing = [];
+
+var walk = function (dir, done) {
+    fs.readdir(dir, function (error, list) {
+        if (error) {
+            return done(error);
+        }
+
+        var i = 0;
+
+        (function next () {
+            var file = list[i++];
+
+            if (!file) {
+                return done(null);
+            }
+            
+            file = dir + '/' + file;
+            
+            fs.stat(file, function (error, stat) {
+        
+                if (stat && stat.isDirectory()) {
+                    walk(file, function (error) {
+                        next();
+                    });
+                } else {
+                    // do stuff to file here
+                    if(file.includes('.js') || file.includes('.ts') || file.includes('.jsx') || file.includes('.tsx') || file.includes('.vue') ) {
+                    totalFiles++;
+                    readAndStripFile(file)
+                    }
+                    next();
+                }
+            });
+        })();
+    });
+};
+
+function getData(srcPath) { 
+fs.readFile(srcPath, 'utf8', function (err, data) {
+        if (err) throw err;
+            // console.log(`reading ${srcPath}`);
+        
+        const fileContents = String(data);
+        const regex = /console\.log\(([^)]+)\);?/igm
+        const formatted = fileContents.replace(regex," ");
+        
+          fs.writeFile (srcPath, formatted, function(err) {
+        if (err) throw err;
+            console.log(`stripped ${srcPath}`);
+        }
+    );
+        }
+    );
+}
+
+async function readAndStripFile(path) {
+     await getData(path);  
+}
+
+module.exports = { strip: walk }
+
+walk(walkPath, function(error) {
+    if (error) {
+        throw error;
+    } else {
+            console.log(totalFiles)
+    }
+});
